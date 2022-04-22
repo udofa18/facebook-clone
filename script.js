7ff53f434f8e2f6d123b11968f8bab239606e9cc
iamkelv <moseskelvin4u@gmail.com>
2022-04-22 22:01:30 +0100

    main

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let nums = [1, 1, 0, 1, 0, 0, 0, 0, 1];
-let newArr = Object(nums[0]);
-let consVal = [];
-nums.forEach((i) => {
-  if (newArr == i) {
-    consVal.push(i);
+let arr = [8, 7, 5, 8, 0, 5, 7, 3, 6, 8];
+
+function mostFreqNum(nums) {
+  let count = 0;
+  let mostFreqNum;
+  let maxCount = 1;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        count++;
+        if (maxCount < count) {
+          maxCount = count;
+          mostFreqNum = nums[i];
+        }
+      }
+    }
+    count = 0;
   }
-});
-console.log(consVal);
+  console.log(maxCount);
+}
+mostFreqNum(arr);
